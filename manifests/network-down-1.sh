b15ba0bddd037b48dc1fb22d98e6c9ed77d33e53
blockodyssey-kmyoo <kmyoo@blockodyssey.io>
2023-06-30 20:46:39 +0900

    feat: move yamls into manifests, add london genesis

---
+++ manifests/network-down-1.sh
@@ -0,0 +1,4 @@
+kubectl delete -f validator-4
+kubectl delete -f validator-3
+kubectl delete -f validator-2
+kubectl delete -f validator-1
---
+++ manifests/network-up-1.sh
@@ -0,0 +1,3 @@
+kubectl apply -f network/config-toml-configmap.yaml
+kubectl apply -f network/genesis-json-configmap.yaml
+kubectl apply -f bootnode-1
