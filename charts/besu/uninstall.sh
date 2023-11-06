264ecc603b6e64208896254028a94fe228876425
blockodyssey-kmyoo <kmyoo@blockodyssey.io>
2023-11-06 10:44:37 +0900

    chore: test suite clean pvc

--- charts/besu/uninstall.sh
+++ charts/besu/uninstall.sh
@@ -3,3 +3,8 @@ helm uninstall besu-validator-1
 helm uninstall besu-validator-2
 helm uninstall besu-validator-3
 helm uninstall besu-validator-4
+kubectl delete pvc data-besu-rpc-1-0
+kubectl delete pvc data-besu-validator-1-0
+kubectl delete pvc data-besu-validator-2-0
+kubectl delete pvc data-besu-validator-3-0
+kubectl delete pvc data-besu-validator-4-0
