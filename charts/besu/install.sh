41d74efd531fda95209c675576d636431438508a
blockodyssey-kmyoo <kmyoo@blockodyssey.io>
2023-11-11 15:16:27 +0900

    chore: test suite scripts kube context

--- charts/besu/install.sh
+++ charts/besu/install.sh
@@ -1,5 +1,5 @@
-helm install besu-rpc-1 . --values ./values/besu-rpc-1.yaml
-helm install besu-validator-1 . --values ./values/besu-validator-1.yaml
-helm install besu-validator-2 . --values ./values/besu-validator-2.yaml
-helm install besu-validator-3 . --values ./values/besu-validator-3.yaml
-helm install besu-validator-4 . --values ./values/besu-validator-4.yaml
+helm --kube-context kind-kind install besu-rpc-1 . --values ./values/besu-rpc-1.yaml
+helm --kube-context kind-kind install besu-validator-1 . --values ./values/besu-validator-1.yaml
+helm --kube-context kind-kind install besu-validator-2 . --values ./values/besu-validator-2.yaml
+helm --kube-context kind-kind install besu-validator-3 . --values ./values/besu-validator-3.yaml
+helm --kube-context kind-kind install besu-validator-4 . --values ./values/besu-validator-4.yaml
--- charts/besu/uninstall.sh
+++ charts/besu/uninstall.sh
@@ -1,10 +1,10 @@
-helm uninstall besu-rpc-1
-helm uninstall besu-validator-1
-helm uninstall besu-validator-2
-helm uninstall besu-validator-3
-helm uninstall besu-validator-4
-kubectl delete pvc data-besu-rpc-1-0
-kubectl delete pvc data-besu-validator-1-0
-kubectl delete pvc data-besu-validator-2-0
-kubectl delete pvc data-besu-validator-3-0
-kubectl delete pvc data-besu-validator-4-0
+helm --kube-context kind-kind uninstall besu-rpc-1
+helm --kube-context kind-kind uninstall besu-validator-1
+helm --kube-context kind-kind uninstall besu-validator-2
+helm --kube-context kind-kind uninstall besu-validator-3
+helm --kube-context kind-kind uninstall besu-validator-4
+kubectl --context kind-kind delete pvc data-besu-rpc-1-0
+kubectl --context kind-kind delete pvc data-besu-validator-1-0
+kubectl --context kind-kind delete pvc data-besu-validator-2-0
+kubectl --context kind-kind delete pvc data-besu-validator-3-0
+kubectl --context kind-kind delete pvc data-besu-validator-4-0
